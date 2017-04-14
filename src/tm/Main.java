4252de61ddf4be5b1d86f7e52fdcb3dd0b3875fd
Hydroxyl <idnarosirrah@gmail.com>
2017-04-14 06:12:18 -0500

    Added transition for 2 keywords

--- src/tm/Main.java
+++ src/tm/Main.java
@@ -4,8 +4,11 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		TuringMachine TM1 = MachineLibrary.DocumentChecker();
+		String input = "registration";
+		input = input.replaceAll("\\s+","");
 		
-		boolean done = TM1.Run("010000110101#010000110101", false);
+		
+		boolean done = TM1.Run(input, false);
 		if (done==true)
 		{
 			System.out.println("The input was accepted.");
